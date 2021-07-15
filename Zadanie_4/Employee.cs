70daf1cc5a4943c95ec2f6be343487cca081e89f
Leslaw1589 <jokerostyczny@gmail.com>
2021-07-15 17:08:42 +0200

    Add project files.

---
+++ Zadanie_4/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Employee
+    {
+        String imie, nazwisko;
+        double stawka, wyplata, stankonta;
+
+        public Employee(String name, String surname, double wage)
+        {
+            imie = name;
+            nazwisko = surname;
+            stawka = wage;
+        }
+
+        public void RegisterTime(double czas)
+        {
+            if (czas < 8)  wyplata += czas * stawka;
+            else wyplata += (czas - 8) * (stawka * 2) + (8 * stawka);
+        }
+
+        public void PaySalary()
+        {
+            stankonta += wyplata;
+            Console.WriteLine("Wyplacono pracownikowi {0}. Stan konta pracownika to: {1}", wyplata, stankonta);
+            wyplata = 0;
+        }
+
+    }
+}
---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Jan", "Nowak", 100.0);
+
+            emp1.RegisterTime(5);
+            emp1.RegisterTime(10);
+            emp1.RegisterTime(7.5);
+            emp1.PaySalary();
+
+            emp1.RegisterTime(11);
+            emp1.PaySalary();
+
+            Console.ReadKey();
+        }
+    }
+}
